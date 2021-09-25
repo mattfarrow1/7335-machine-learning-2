a92bbb8efceb785d578683b290db83948fdaae0d
Matt Farrow <matt@formalizedcuriosity.net>
2021-09-25 16:44:20 -0500

    final rankings

--- rankings.py
+++ rankings.py
@@ -1,1 +1,54 @@
 ['Matt Farrow', 'A. Bravo', 'N. Brown', 'Y. Zhang', 'G. Kodi', 'K. Chang', 'Philip', 'C. Kim', 'S. Fogelman', 'S. Fogelman', 'K. Ghimire', 'R. Talk', 'Andrew Larsen', 'M. Palanisamy', 'E. Lull', 'S. Garcia de Alford'],
+
+# A. Bravo
+# Bayesian vs. Frequentist Approach in Probability and Machine Learning
+# https://vimeo.com/478297493
+
+# N. Brown
+# Distributed Representations
+
+# Y. Zhang
+# Normalization Methods in Deep Learning
+# https://vimeo.com/478700010
+
+# G. Kodi
+# Math behind SVM Kernel
+# https://vimeo.com/378037194
+
+# K. Chang
+# Collaborative vs. Content Filtering: User Targeting
+# https://vimeo.com/409343548
+
+# Philip
+# BOP: Benchmark for 6D Object Pose Estimation
+# https://youtu.be/k3hwzoFEP7A
+# https://www.youtube.com/watch?v=D3Ii0ospDeM
+
+# C. Kim
+# Mini-Batch Gradient Descent
+# https://vimeo.com/332599158
+
+# S. Fogelman
+# Working with Imbalanced Datasets
+# https://vimeo.com/user113038999/review/408735455/321f76f619
+
+# K. Ghimire
+# Word2Vec Continuous Bag of Words (CBOW) and Skip-gram
+# https://youtu.be/CsgiVnW401c
+
+# R. Talk
+# Pooling in Convolutional Neural Network
+# https://vimeo.com/331347019
+
+# Andrew Larsen
+# Four Color Theorem
+
+# M. Palanisamy
+# Reinforcement Learning
+# https://vimeo.com/332024768
+
+# S. Garcia de Alford
+# Long Short Term Memory, an Overview
+# https://vimeo.com/377010120
+
+['Matt Farrow','K. Ghimire','R. Talk','C. Kim','Philip','N. Brown','K. Chang','Y. Zhang','S. Garcia de Alford','A. Bravo','G. Kodi','M. Palanisamy']
